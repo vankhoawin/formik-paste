1fe344aa8ad44d03dcdb3ad03d4e0c1a5e205051
Van Nguyen <vanguyen@twilio.com>
2020-08-09 12:41:05 -0700

    test(forminput): add test case for number input validation

--- src/components/FormInput/FormInput.stories.tsx
+++ src/components/FormInput/FormInput.stories.tsx
@@ -1,5 +1,5 @@
 // import { action } from '@storybook/addon-actions';
-import { Button, FormLabel } from '@twilio-paste/core';
+import { Button, FormLabel, FormHelpText } from '@twilio-paste/core';
 import { Theme } from '@twilio-paste/theme';
 import { Formik } from 'formik';
 import * as React from 'react';
@@ -13,25 +13,64 @@ export default {
 export const Basic: React.FC = () => (
   <Theme.Provider theme="default">
     <Formik
-      initialValues={{ checkbox: true }}
+      initialValues={{ emailAddress: '' }}
       onSubmit={(values) => {
-        // eslint-disable-next-line no-alert
         window.alert(JSON.stringify(values));
       }}
     >
       {({ handleSubmit }) => (
         <form onSubmit={handleSubmit}>
-          <FormLabel htmlFor="email_address">Email Address</FormLabel>
+          <FormLabel htmlFor="emailAddress">Email Address</FormLabel>
+          <FormInput id="emailAddress" name="emailAddress" type="email" placeholder="example@twilio.com" />
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </form>
+      )}
+    </Formik>
+  </Theme.Provider>
+);
+
+export const NumberInputWithValidation: React.FC = () => (
+  <Theme.Provider theme="default">
+    <Formik
+      initialValues={{ positiveNumber: undefined }}
+      onSubmit={(values) => {
+        window.alert(JSON.stringify(values));
+      }}
+      validate={(values) => {
+        const errors: Record<string, string> = {};
+        const positiveNumber = Number(values.positiveNumber);
+
+        if (isNaN(positiveNumber)) {
+          errors.positiveNumber = 'Input is not a number';
+        } else if (positiveNumber < 0) {
+          errors.positiveNumber = 'Number is not positive';
+        }
+
+        return errors;
+      }}
+    >
+      {({ errors, handleSubmit }) => (
+        <form onSubmit={handleSubmit}>
+          <FormLabel htmlFor="positiveNumber">Positive Number</FormLabel>
 
           <FormInput
-            aria-describedby="email_error_help_text"
-            id="email_address"
-            name="email_address"
-            type="email"
+            aria-describedby="positiveNumberErrorHelpText"
+            id="positiveNumber"
+            name="positiveNumber"
+            type="text"
             placeholder="example@twilio.com"
           />
 
-          <Button variant="primary" type="submit">
+          {errors.positiveNumber && (
+            <FormHelpText id="positiveNumberErrorHelpText" variant="error">
+              {errors.positiveNumber}
+            </FormHelpText>
+          )}
+
+          <Button variant="primary" type="submit" disabled={Boolean(errors.positiveNumber)}>
             Submit
           </Button>
         </form>
