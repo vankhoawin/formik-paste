7ab9eae70bbb987fcbda374b871d12320102bb4e
Van Nguyen <vanguyen@twilio.com>
2020-08-09 12:39:46 -0700

    build: add dependencies, storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,29 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   extends: ['twilio-react', 'twilio-ts'],
+  parserOptions: {
+    project: './tsconfig.lint.json',
+  },
 
   env: {
     node: true,
     browser: true,
     jest: true,
   },
+
+  rules: {
+    'react/jsx-pascal-case': 0,
+    'import/no-unused-modules': 0,
+  },
+
+  overrides: [
+    {
+      files: ['*.stories.tsx'],
+      rules: {
+        'no-alert': 0,
+        'react/display-name': 0,
+        'react/no-multi-comp': 0,
+      },
+    },
+  ],
 };
---
+++ .storybook/main.js
@@ -0,0 +1,18 @@
+module.exports = {
+  stories: ['../src/components/**/*.stories.tsx'],
+  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: require.resolve('ts-loader'),
+        },
+      ],
+    });
+    config.resolve.extensions.push('.ts', '.tsx');
+
+    return config;
+  },
+};
