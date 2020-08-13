7120f26cb438ec94f0493b32918e007edafddcb8
Van Nguyen <vanguyen@twilio.com>
2020-08-12 23:11:07 -0700

    chore(storybook): update to storybook v6

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,4 @@
 module.exports = {
   stories: ['../src/**/*.stories.tsx'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links'],
-
-  webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      use: [
-        {
-          loader: require.resolve('ts-loader'),
-        },
-      ],
-    });
-    config.resolve.extensions.push('.ts', '.tsx');
-
-    return config;
-  },
 };
